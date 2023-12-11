0eda358f8a97994a76c564d618d403ccc94fa1fd
Elena Ludina <elenaludina0@gmail.com>
2023-12-11 22:27:44 +0300

    Str_func_1

--- str_func.py
+++ str_func.py
@@ -1,4 +1,10 @@
 s = input()
-def str_func ():
+def str_func():
+"""returns a string in capital letters"""
     str = s.upper()
-    return (str)
+    return str
+def str_func_1():
+"""returns the uppercase first letters of each word in the string"""
+    str_1 = s.title()
+    return str_1
+
